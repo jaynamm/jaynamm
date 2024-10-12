db69ae4dc3c97e053e5d6f521d44d50596919e4e
jaynamm <polky0421@gmail.com>
2024-10-13 00:51:17 +0900

    refactor: moment 사용해서 날짜 변환

--- update_readme.js
+++ update_readme.js
@@ -20,28 +20,24 @@ const parser = new Parser({
   // 최신 5개의 글의 제목과 링크를 추가할 텍스트 생성
   let latestPosts = `
 
-  ✅ Latest Blog Post
+  Latest Blog Post
   ---
   
   `;
 
   for (let i = 0; i < 10 && i < feed.items.length; i++) {
-    console.log(feed.items[i])
     const { title, link, pubDate} = feed.items[i];
 
-    // 문자열을 Date 객체로 변환
-    const dateObj = new Date(pubDate);
-
-    // 날짜를 'YYYY-MM-DD' 형식으로 변환
-    const formattedDate = dateObj.toISOString().split('T')[0];
+    const moment = require('moment');
+    const formattedDate = moment(pubDate).format('YYYY/MM/DD');
 
     latestPosts += `- [${formattedDate} - ${title}](${link})\n`;
   }
  
   // 기존 README.md에 최신 블로그 포스트 추가
-  const newReadmeContent = readmeContent.includes("✅ Latest Blog Post")
+  const newReadmeContent = readmeContent.includes("Latest Blog Post")
     ? readmeContent.replace(
-        /✅ Latest Blog Post[\s\S]*?(?=\n\n## |\n$)/,
+        /Latest Blog Post[\s\S]*?(?=\n\n## |\n$)/,
         latestPosts
       )
     : readmeContent + latestPosts;
