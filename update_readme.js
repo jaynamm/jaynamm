27788dadb6053166fc156a05670717d6c02e7392
jaynamm <polky0421@gmail.com>
2024-10-13 00:56:58 +0900

    fix: syntax error

--- update_readme.js
+++ update_readme.js
@@ -19,7 +19,10 @@ const parser = new Parser({
   const feed = await parser.parseURL("https://jaynam.tistory.com/rss");
  
   // 최신 5개의 글의 제목과 링크를 추가할 텍스트 생성
-  let latestPosts = "Latest Blog Post\n---";
+  let latestPosts = `
+  Latest Blog Post
+  ---
+  `;
 
   for (let i = 0; i < 10 && i < feed.items.length; i++) {
     const { title, link, pubDate} = feed.items[i];
